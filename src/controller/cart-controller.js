9f59166d97dfa0d51d7841726944575110d13f04
catmellowz <giift19@gmail.com>
2023-02-06 16:35:02 +0700

    amount item in cart

--- src/controller/cart-controller.js
+++ src/controller/cart-controller.js
@@ -4,19 +4,34 @@ const { Cart, Service } = require('../models');
 
 exports.addCart = async (req, res, next) => {
   try {
-    const service = await Service.findOne({
-      where: {
-        [Op.or]: {
-          id: req.body.serviceId,
-        },
-      },
-    });
-
+    // test find service
+    // const service = await Service.findOne({
+    //   where: {
+    //     [Op.or]: {
+    //       id: req.body.serviceId,
+    //     },
+    //   },
+    // });
+    //create service from font
     const cart = await Cart.create({
-      serviceId: service.id,
+      serviceId: req.body.serviceId,
       userId: req.user.id,
     });
+
     res.status(201).json({ cart });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAmount = async (req, res, next) => {
+  try {
+    const amount = await Cart.count({
+      where: {
+        userId: req.user.id,
+      },
+    });
+    res.status(201).json({ amount });
   } catch (err) {
     next(err);
   }
--- src/routes/cart-router.js
+++ src/routes/cart-router.js
@@ -5,5 +5,6 @@ const CartController = require('../controller/cart-controller');
 const router = express.Router();
 
 router.post('/cart', CartController.addCart);
+router.get('/cart', CartController.getAmount);
 
 module.exports = router;
