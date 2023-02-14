557955923ba14f387ef7f97962bf1661c665a50f
catmellowz <giift19@gmail.com>
2023-02-14 15:26:13 +0700

    fix

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 
 module.exports = (err, req, res, next) => {
   console.log(chalk.redBright.bold.italic(err));
-  console.log(err);
+  // console.log(err);
 
   if (err.name === 'ValidationError') {
     err.statusCode = 400;
@@ -10,8 +10,6 @@ module.exports = (err, req, res, next) => {
     err.statusCode = 401;
   } else if (err.name === 'JsonWebTokenError') {
     err.statusCode = 401;
-  } else if (err.name === 'SequelizeForeignKeyConstrainError') {
-    err.statusCode = 400;
   }
   res.status(err.statusCode || 500).json({ message: err.message });
 };
